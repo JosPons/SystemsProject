755bdfa2fd38423c44f5cff4027f2d8b435d68fa
Joseph <demisjoseph@hotmail.com>
2020-10-19 20:27:35 +0300

    Update .gitignore file

---
+++ cmake-build-debug/CMakeFiles/SystemsProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SystemsProject.dir/main.c.o"
+  "SystemsProject.exe"
+  "SystemsProject.pdb"
+  "libSystemsProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SystemsProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
