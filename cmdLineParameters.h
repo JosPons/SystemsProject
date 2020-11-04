241e449127aa3747b00ccde72f6cad0487a20036
Joseph <demisjoseph@hotmail.com>
2020-11-04 03:32:35 +0200

    Program parameters
    - Add structure for initialize and print of command line parameters.

---
+++ cmdLineParameters.h
@@ -0,0 +1,16 @@
+#ifndef SYSTEMSPROJECT_CMDLINEPARAMETERS_H
+#define SYSTEMSPROJECT_CMDLINEPARAMETERS_H
+
+#include <stdio.h>
+
+
+typedef struct programParameters_t
+{
+  char inputFilePath[FILENAME_MAX];
+  char queryFilePath[FILENAME_MAX];
+} programParameters_t;
+
+void initializeProgramParameters();
+void printProgramParameters();
+
+#endif //SYSTEMSPROJECT_CMDLINEPARAMETERS_H
